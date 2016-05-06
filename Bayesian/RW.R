5916364e4eb5a0772666359f83462e41fd94e316
bw4sz <benweinstein2010@gmail.com>
2016-05-05 19:49:40 -0700

    random walk complete

--- Bayesian/RW.R
+++ Bayesian/RW.R
@@ -4,14 +4,14 @@ cat("
     #Process Model
     for(x in 2:steps){
     y[x,] ~ dmnorm(y[x-1,],iSigma)
+
+    #Generate prediction
+    ynew[x,1:2] ~ dmnorm(y[x-1,1:2],iSigma)
     }
+
     #Priors
     iSigma~dwish(R,3)
 
-    #Generate prediction
-    ynew[1,1]<-0
-    ynew[1,2]<-0
-    ynew[x,] ~ dmnorm(y[x-1,],iSigma)
     }"
     ,fill=TRUE)
 sink()
