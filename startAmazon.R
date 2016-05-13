3329b340a42b46ba59d18e21a93c836a357702f9
bw4sz <benweinstein2010@gmail.com>
2016-05-13 09:40:03 -0700

    behavior plots

--- startAmazon.R
+++ startAmazon.R
@@ -2,11 +2,6 @@
 install.packages(c("chron","raster","dplyr","devtools"))
 devtools::install_github('caseyyoungflesh/potools')
 
-#clone from github
-
-system("git clone https://github.com/bw4sz/Whales.git")
-system("cd Whales")
-
 
 
 
