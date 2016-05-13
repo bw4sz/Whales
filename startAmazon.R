0321a217b04bc7119d2962923c363a28e4f99b3f
bw4sz <benweinstein2010@gmail.com>
2016-05-13 07:43:56 -0700

    changing plots for dplot

---
+++ startAmazon.R
@@ -0,0 +1,12 @@
+#Source script for Amazon 
+install.packages(c("chron","raster","dplyr","devtools"))
+devtools::install_github('caseyyoungflesh/potools')
+
+#clone from github
+
+system("git clone https://github.com/bw4sz/Whales.git")
+system("cd Whales")
+
+
+
+
