a3fb6e4c90b5edfd8118777fca4a69e0f54e641b
bw4sz <benweinstein2010@gmail.com>
2016-05-18 21:33:35 -0700

    amazon names after instance ID, runs script, then kills itself

--- Amazon/Job.bash
+++ Amazon/Job.bash
@@ -6,12 +6,16 @@ cd Whales
 #git pull to make sure we are at HEAD
 git pull
 
+#make new branch
+#name it the instance ID
+iid=$(ec2metadata --instance-id)
+
+git checkout -b $iid
+
 #render script
 Rscript -e "rmarkdown::render('MutliSpeciesHMM.Rmd')"
 
 #push results
 git add --all
 git commit -m "ec2 run complete"
-git push
-
-
+git push -u origin $iid
