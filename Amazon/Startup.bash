fe9cfee0cd8680441c4969135247b0cffcd083ff
bw4sz <benweinstein2010@gmail.com>
2016-06-15 16:43:22 -0700

    no hup works

--- Amazon/Startup.bash
+++ Amazon/Startup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # spawn instance and store id
-instance_id=$(aws ec2 run-instances --image-id ami-5fa96c3f --security-group-ids sg-890a37ed --count 1 --instance-type t2.medium --key-name rstudio --instance-initiated-shutdown-behavior terminate --query 'Instances[0].{d:InstanceId}' --output text)
+instance_id=$(aws ec2 run-instances --image-id ami-5fa96c3f --security-group-ids sg-890a37ed --count 1 --instance-type t2.micro --key-name rstudio --instance-initiated-shutdown-behavior terminate --query 'Instances[0].{d:InstanceId}' --output text)
 
 # wait until instance is up and running
 aws ec2 wait instance-running --instance-ids $instance_id
@@ -16,4 +16,4 @@ sleep 60
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i '/c/Users/Ben/.ssh/rstudio.pem' /c/Users/Ben/Documents/Whales/Amazon/Job.bash ubuntu@$dns:~
 
 # run job script on instance, don't wait for finish and disconnect terminal
-nohup ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "/c/Users/Ben/.ssh/rstudio.pem" ubuntu@$dns "bash ~/Job.bash" &
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "/c/Users/Ben/.ssh/rstudio.pem" ubuntu@$dns "bash ~/Job.bash"
