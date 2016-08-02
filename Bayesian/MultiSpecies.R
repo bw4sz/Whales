a3da948139a6cb80df0fc00ebd37d5a410e8c622
bw4sz <benweinstein2010@gmail.com>
2016-08-01 19:36:01 -0700

    works

--- Bayesian/MultiSpecies.R
+++ Bayesian/MultiSpecies.R
@@ -15,42 +15,42 @@ cat("
     argos_cov[2,2] <- argos_alpha
     
     for(i in 1:ind){
-    for(g in 1:tracks[i]){
-    
-    ## Priors for first true location
-    #for lat long
-    y[i,g,1,1:2] ~ dmnorm(argos[i,g,1,1,1:2],argos_prec)
-    
-    #First movement - random walk.
-    y[i,g,2,1:2] ~ dmnorm(y[i,g,1,1:2],iSigma)
-    
-    ###First Behavioral State###
-    state[i,g,1] ~ dcat(lambda[]) ## assign state for first obs
-    
-    #Process Model for movement
-    for(t in 2:(steps[i,g]-1)){
-    
-    #Behavioral State at time T
-    logit(phi[i,g,t,1]) <- alpha_mu[state[i,g,t-1]] + beta_mu * ocean[i,g,t] + beta2_mu * coast[i,g,t]
-    phi[i,g,t,2] <- 1-phi[i,g,t,1]
-    state[i,g,t] ~ dcat(phi[i,g,t,])
-    
-    #Turning covariate
-    #Transition Matrix for turning angles
-    T[i,g,t,1,1] <- cos(theta[state[i,g,t]])
-    T[i,g,t,1,2] <- (-sin(theta[state[i,g,t]]))
-    T[i,g,t,2,1] <- sin(theta[state[i,g,t]])
-    T[i,g,t,2,2] <- cos(theta[state[i,g,t]])
-    
-    #Correlation in movement change
-    d[i,g,t,1:2] <- y[i,g,t,] + gamma[state[i,g,t]] * T[i,g,t,,] %*% (y[i,g,t,1:2] - y[i,g,t-1,1:2])
-    
-    #Gaussian Displacement
-    y[i,g,t+1,1:2] ~ dmnorm(d[i,g,t,1:2],iSigma)
-    }
-    
+      for(g in 1:tracks[i]){
+
+        ## Priors for first true location
+        #for lat long
+        y[i,g,1,1:2] ~ dmnorm(argos[i,g,1,1,1:2],argos_prec)
+
+        #First movement - random walk.
+        y[i,g,2,1:2] ~ dmnorm(y[i,g,1,1:2],iSigma)
+        
+        ###First Behavioral State###
+        state[i,g,1] ~ dcat(lambda[]) ## assign state for first obs
+        
+        #Process Model for movement
+        for(t in 2:(steps[i,g]-1)){
+        
+        #Behavioral State at time T
+        logit(phi[i,g,t,1]) <- alpha_mu[state[i,g,t-1]] + beta_mu[state[i,g,t-1]] * ocean[i,g,t] + beta2_mu[state[i,g,t-1]] * coast[i,g,t]
+        phi[i,g,t,2] <- 1-phi[i,g,t,1]
+        state[i,g,t] ~ dcat(phi[i,g,t,])
+            
+        #Turning covariate
+        #Transition Matrix for turning angles
+        T[i,g,t,1,1] <- cos(theta[state[i,g,t]])
+        T[i,g,t,1,2] <- (-sin(theta[state[i,g,t]]))
+        T[i,g,t,2,1] <- sin(theta[state[i,g,t]])
+        T[i,g,t,2,2] <- cos(theta[state[i,g,t]])
+        
+        #Correlation in movement change
+        d[i,g,t,1:2] <- y[i,g,t,] + gamma[state[i,g,t]] * T[i,g,t,,] %*% (y[i,g,t,1:2] - y[i,g,t-1,1:2])
+        
+        #Gaussian Displacement
+        y[i,g,t+1,1:2] ~ dmnorm(d[i,g,t,1:2],iSigma)
+      }
+
     #Final behavior state
-    logit(phi[i,g,steps[i,g],1]) <- alpha_mu[state[i,g,steps[i,g]-1]] + beta_mu * ocean[i,g,steps[i,g]] + beta2_mu * coast[i,g,steps[i,g]]
+    logit(phi[i,g,steps[i,g],1]) <- alpha_mu[state[i,g,steps[i,g]-1]] + beta_mu[state[i,g,steps[i,g]-1]] * ocean[i,g,steps[i,g]] + beta2_mu[state[i,g,steps[i,g]-1]] * coast[i,g,steps[i,g]]
     phi[i,g,steps[i,g],2] <- 1-phi[i,g,steps[i,g],1]
     state[i,g,steps[i,g]] ~ dcat(phi[i,g,steps[i,g],])
     
@@ -62,16 +62,16 @@ cat("
     # loops over observed locations within interval t
     for(u in 1:idx[i,g,t]){ 
       zhat[i,g,t,u,1:2] <- (1-j[i,g,t,u]) * y[i,g,t-1,1:2] + j[i,g,t,u] * y[i,g,t,1:2]
-      
-      #for each lat and long
-      #argos error
-      argos[i,g,t,u,1:2] ~ dmnorm(zhat[i,g,t,u,1:2],argos_prec)
+
+        #for each lat and long
+          #argos error
+          argos[i,g,t,u,1:2] ~ dmnorm(zhat[i,g,t,u,1:2],argos_prec)
           }
         }
       }
     }
     ###Priors###
-    
+
     #Process Variance
     iSigma ~ dwish(R,2)
     Sigma <- inverse(iSigma)
@@ -88,27 +88,46 @@ cat("
     
     ##Move persistance
     # prior for gamma (autocorrelation parameter) in state 1
-    gamma[1] ~ dunif(0.5,1)
+    gamma[1] ~ dbeta(0.5,1)
     
     # prior for gamma in state 2
     gamma[2] ~ dunif(0,0.5)
-
+    
+    ##Behavioral States
+    # Following lunn 2012 p85
+    
     #Hierarchical structure
     #Intercepts
     alpha_mu[1] ~ dnorm(0,0.386)
     alpha_mu[2] ~ dnorm(0,0.386)
-    
+
+    #Variance
+    alpha_tau[1] ~ dt(0,1,1)I(0,)
+    alpha_tau[2] ~ dt(0,1,1)I(0,)
+
     #Slopes
     ## Ocean Depth
-    beta_mu ~ dnorm(0,0.386)
+    beta_mu[1] ~ dnorm(0,0.386)
+    beta_mu[2] ~ dnorm(0,0.386)
 
     # Distance coast
-    beta2_mu ~ dnorm(0,0.386)
+    beta2_mu[1] ~ dnorm(0,0.386)
+    beta2_mu[2] ~ dnorm(0,0.386)
 
+    #Variance
+    #Ocean
+    beta_tau[1] ~ dt(0,1,1)I(0,)
+    beta_tau[2] ~ dt(0,1,1)I(0,)
+    
+    #Coast
+    beta2_tau[1] ~ dt(0,1,1)I(0,)
+    beta2_tau[2] ~ dt(0,1,1)I(0,)
+    
+    
     #Probability of behavior switching 
     lambda[1] ~ dbeta(1,1)
     lambda[2] <- 1 - lambda[1]
-    
+
     ##Argos priors##
     #longitudinal argos error
     argos_sigma ~ dunif(0,10)
@@ -119,7 +138,7 @@ cat("
     #correlation in argos error
     rho ~ dunif(-1, 1)
     
-    
+
     }"
     ,fill=TRUE)
 sink()
