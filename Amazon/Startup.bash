d58c428eece7fde4196198149be269d6fee17956
bw4sz <benweinstein2010@gmail.com>
2016-09-14 11:23:36 -0700

    forgot the cpu dimensions

--- Amazon/Startup.bash
+++ Amazon/Startup.bash
@@ -10,7 +10,7 @@ aws ec2 wait instance-running --instance-ids $instance_id
 aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=Whales
 
 #cloudwatch monitor
-aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description "Alarm when CPU drops below 2 over 10 minutes%" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 2 --comparison-operator LessThanThreshold  --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:477056371121:Instance_is_idle --unit Percent
+aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description "Alarm when CPU drops below 2 over 10 minutes%" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 2 --comparison-operator LessThanThreshold  --dimensions Name=InstanceId,Value=$instance_id --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:477056371121:Instance_is_idle --unit Percent
 aws cloudwatch put-metric-alarm --alarm-name memory --alarm-description "Alarm when Memory rate exceeds 90%%" --metric-name MemoryUtilization --namespace System/Linux --statistic Average --period 300 --threshold 90 --comparison-operator GreaterThanThreshold  --dimensions Name=InstanceId,Value=$instance_id --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:477056371121:Instance_is_idle --unit Percent
 
 # retrieve public dns
