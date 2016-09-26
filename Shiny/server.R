87b9f507022de7c9026643f22b267c7a838b3b8a
bw4sz <benweinstein2010@gmail.com>
2016-09-25 17:27:28 -0700

    shiny intergtation

--- Shiny/server.R
+++ Shiny/server.R
@@ -9,34 +9,50 @@ server <- function(input, output, session) {
   #Read in data
   d<-read.csv("WhaleDat.csv")
   
+  ##Base map
   wrapper <- function(df) {
     df  %>% select(x,y) %>% as.data.frame %>% Line %>% list %>% return
   }
   
-  y <-  d %>% group_by(Animal) %>%
-    do(res = wrapper(.)) 
   
-  # and now assign IDs (since we can't do that inside dplyr easily)
-  ids = 1:dim(y)[1]
-  data<-SpatialLines(
-    mapply(x = y$res, ids = ids, FUN = function(x,ids) {Lines(x,ID=ids)})
-  ,proj4string=CRS("+proj=longlat +datum=WGS84"))
-  
-  #Base map
-  m<-leaflet() %>% addTiles() 
-  
-  #By Year
-    dinfo<-d %>% distinct(Animal) %>% select(Animal,Year)
+  filteredData<-reactive({
+    
+    #filter based on slider
+    #year
+    if(input$year=="All"){yr<-c(2012:2016)} else{yr<-input$year}
+    #Month
+    if(input$month=="All"){mn<-month.name} else{mn<-input$month}
+    
+    y <-  d %>% group_by(Animal) %>% filter(Year %in% yr,Month %in% mn) %>%
+      do(res = wrapper(.))
+    
+    #break if there is no data
+    if(nrow(y)==0){return(NULL)}
+    
+    # and now assign IDs (since we can't do that inside dplyr easily)
+    ids = 1:dim(y)[1]
+    data<-SpatialLines(
+      mapply(x = y$res, ids = ids, FUN = function(x,ids) {Lines(x,ID=ids)})
+      ,proj4string=CRS("+proj=longlat +datum=WGS84"))
+    
+    dinfo<-d %>% filter(Year %in% yr, Month %in% mn) %>% distinct(Animal) 
     spl<-SpatialLinesDataFrame(data,dinfo)
     
-    #Associate points data
-    spp<-SpatialPointsDataFrame(data=d,cbind(d$x,d$y),proj4string=CRS("+proj=longlat +datum=WGS84"))
-    
-    #points
-    #m <- leaflet() %>% addTiles() %>% addMarkers(data=spp,lng=~x,lat=~x,clusterId = ~Animal,clusterOptions = markerClusterOptions())
-    
-    binpal <- colorBin("RdYlBu", dinfo$Year, 6, pretty = FALSE)
-
-  m <- leaflet() %>% addTiles() %>% addPolylines(data=spl,color=~binpal(Year),popup=~as.character(Animal),weight=2)
+    return(spl)
+  })
+  
+  m <- leaflet(d) %>% addTiles() %>% fitBounds(~min(x),~min(y),~max(x),~max(y))
+observe({
+  
+  #check if there is data for the month year combination
+  addD<-filteredData()
+  if(is.null(addD)){
+    #clear screen
+    leafletProxy("mymap",data=addD) %>% clearShapes()
+  }else{
+    #add tracks
+    proxy<-leafletProxy("mymap",data=addD) %>% clearShapes() %>% addPolylines(popup=~as.character(Animal),weight=2)  
+  }
+})
   output$mymap <- renderLeaflet(m)
-}
+  }
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -3,11 +3,23 @@ library(leaflet)
 
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
+d<-read.csv("WhaleDat.csv")
 
 ui <- fluidPage(
-  titlePanel("Antarctic Humpback Whales"),
-  p("Since 2012, we tagged adult Humpback whales as they forage off the coast of the West Antarctic Penisula. This projects allows us a greater insight into their movemements, behaviors, and potential for conflict with anthropogenic sources."),
-  mainPanel(leafletOutput("mymap")),
-  img(src = "stockimage.png", height = 150, width = 150),
-  sidebarPanel("Color",actionButton("year", "Year"),actionButton("month", "Month")),
-  p("Text describing the project"))
+  titlePanel("Tracking Humpback Whales"),
+  p("Since 2012, we haved tagged adult Humpback whales as they forage off the coast of the West Antarctic Penisula. This projects allows us a greater insight into their movemements, behaviors, and potential for conflict with anthropogenic sources."),
+  
+  #main panel
+  mainPanel(leafletOutput("mymap",height=525),width=10),
+#side bar
+  sidebarPanel(width=2,selectInput("year","Year",c("All",unique(d$Year))),
+  selectInput("month","Month",c("All",month.name[month.name %in% d$Month])),
+  img(src = "stockimage.png", height = 225, width = 150)),
+  p("Please note that the map may take a few seconds to load"),
+  br(),
+  h1("Why Study Whales?"),
+  p("Humpback whales are the largest marine predator commonly found in Antarctic Waters. By studying their movement we gain insight into their recovery from near extinction due to historic harvest. Humpbacks feed primarily on Antarctic Krill, and are important bellweathers for the health and condition of the Antarctic Marine Ecosystem.In the Antarctic, the intersection between commercial fishing and marine mammals continues to grow as fishing pressure increases and vessels move into new, and in some cases, previously inaccessibly environments. The Antarctic Krill (Euphausia superba) fishery is the largest in the Southern Ocean (>300,000 metric tons annually) and operates in ice-free areas during the austral summer. Declining sea ice in local fishing areas has led to a southward expansion and increased the potential for negative impacts on fragile polar marine ecosystems. The expansion of fisheries can have negative consequences for marine mammal population persistence through direction competition for resources, ship-strikes, soundscape effects, and general avoidance of productive feeding areas"),
+  h3("Contact Us"),
+  h4("Ben Weinstein"),p("Ben is a quantitative ecologist and data sceintist at Marine Mammal Research Center, Oregon State University. Ben enjoys appying new technology to ecology and biodiversity conservation."),
+  h4("Ari Friedlaender"),p("Ari is an Assistant Professor at Oregon State University, and has been studying whales for ten years. Ari can be found out to sea everyday that weather and grant applications allow.")
+)
